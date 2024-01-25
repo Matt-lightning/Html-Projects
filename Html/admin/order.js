8cb8e1fd45c16f36b89f34d0a644695612caf7bb
Matt-lightning <98404016+Matt-lightning@users.noreply.github.com>
2024-01-25 14:11:46 -0600

    Add files via upload

---
+++ Html/admin/order.js
@@ -0,0 +1,21 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: 77579,
+        paymentStatus: 'Due',
+        status: 'Pending',
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: 77349,
+        paymentStatus: 'Refunded',
+        status: 'Declined',
+
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: 77201,
+        paymentStatus: 'Paid',
+        status: 'Active',
+    },
+]
